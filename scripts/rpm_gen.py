d1de75f022e540b53861c24837a5809076f7d76e
sattysri <sattysri@gmail.com>
2017-06-13 19:06:03 +0000

    Updated rpm_gen.py script

--- scripts/rpm_gen.py
+++ scripts/rpm_gen.py
@@ -9,28 +9,35 @@ import argparse
 import sys
 import shutil
 
-# Export environment
-env_file = "/enxos-sdk/environment-setup-x86-wrsmllib32-linux"
-output = subprocess.Popen(". " + env_file + ";env", stdout=subprocess.PIPE, shell=True).communicate()[0]
-env = dict((line.split("=", 1) for line in output.splitlines()))
-os.environ.update(env)
-
 # Paths
 if "NXSDK_ROOT" in os.environ:
 	nxsdk_path = os.environ["NXSDK_ROOT"]
 else:
 	nxsdk_path = "/NX-SDK"
 
-rpm_root = "/enxos-sdk/sysroots/x86_64-wrlinuxsdk-linux/usr/src/rpm"
+if "ENXOS_SDK_ROOT" in os.environ:
+	enxos_sdk_path = os.environ["ENXOS_SDK_ROOT"]
+else:
+	enxos_sdk_path = "/enxos-sdk"
+
+# Export environment
+env_file = os.path.join(enxos_sdk_path, "environment-setup-x86-wrsmllib32-linux")
+output = subprocess.Popen(". " + env_file + ";env", stdout=subprocess.PIPE, shell=True).communicate()[0]
+env = dict((line.split("=", 1) for line in output.splitlines()))
+os.environ.update(env)
+
+
+
+rpm_root = os.path.join(enxos_sdk_path, "sysroots/x86_64-wrlinuxsdk-linux/usr/src/rpm")
 
 # Default values for arguments
-default_app_source = nxsdk_path + "/examples"
+default_app_source = os.path.join(nxsdk_path, "examples")
 default_app_desc="Sample description"
-default_app_target = nxsdk_path + "/bin"
+default_app_target = os.path.join(nxsdk_path, "bin")
 default_rel_version = "7.0.3.I6.1"
 
 # Spec file parameters
-custom_spec_file_path = nxsdk_path + "/scripts/template.spec"
+custom_spec_file_path = os.path.join(nxsdk_path, "scripts/template.spec")
 app_name_str = "APP_NAME"
 app_source_str = "APP_SOURCE"
 app_target_str = "APP_TARGET"
@@ -41,28 +48,31 @@ spec_file_params = {}
 # Help string
 
 help_string = "This script generates RPM packages for custom applications built using NX-SDK.\
-Please export the path for NX-SDK as environment variale NXSDK_ROOT before calling the script.\
-If not found, the script assumes NX-SDK to be present at the following location /NX-SDK"
+Please export the path for NX-SDK as environment variable NXSDK_ROOT and \
+ENXOS-SDK as environment variable ENXOS_SDK_ROOT before calling the script.\
+If not found, the script assumes NX-SDK to be present at the following location /NX-SDK \
+and ENXOS-SDK to be present at /enxos-sdk"
 
 def build_rpm(spec_file_path):
 	os.environ["RPM_ROOT"] = rpm_root
 	os.system("rpm -ba " + spec_file_path)
-	rpm_file_path = rpm_root + "/RPMS/x86_64/" + spec_file_params[app_name_str] + "-1.0-" + \
+	rpm_file_name = spec_file_params[app_name_str] + "-1.0-" + \
 		spec_file_params[release_version_str] + ".x86_64.rpm"
+	rpm_file_path = os.path.join(rpm_root, "RPMS/x86_64/", rpm_file_name)
 	return rpm_file_path
 
 
 def copy_files(spec_file_path, rpm_file_path):
-	rpm_dest = nxsdk_path + "/rpm/RPMS/"
+	rpm_dest = os.path.join(nxsdk_path, "rpm/RPMS/")
 	shutil.copy(rpm_file_path, rpm_dest)
-	spec_file_dest = nxsdk_path + "/rpm/SPECS/"
+	spec_file_dest = os.path.join(nxsdk_path, "rpm/SPECS/")
 	shutil.copy(spec_file_path, spec_file_dest)
 	print "\nSPEC file: " + spec_file_dest + os.path.basename(spec_file_path)
 	print "RPM file : " + rpm_dest + os.path.basename(rpm_file_path)
 
 
 def write_spec():
-	spec_file_path = rpm_root + "/SPECS/" + spec_file_params[app_name_str] + ".spec"
+	spec_file_path = os.path.join(rpm_root, "SPECS/", spec_file_params[app_name_str] + ".spec")
 
 	spec_file = open(spec_file_path, 'w')
 	for macro in spec_file_params:
@@ -94,18 +104,19 @@ def main():
 	args = parser.parse_args()
 
 	if not args.use_source_as_target:
-		target = args.target + "/" + args.name
+		target = os.path.join(args.target, args.name)
 	else:
-		target = args.source + "/" + args.name
+		target = os.path.join(args.source, args.name)
 
+	target = os.path.abspath(target)
 	if not os.path.isfile(target):
 		parser.print_help()
 		sys.exit("Target " + target + " not found")
 
 	spec_file_params[app_name_str] = args.name
 	spec_file_params[app_desc_str] = args.description
-	spec_file_params[app_source_str] = args.source
-	spec_file_params[app_target_str] = args.target
+	spec_file_params[app_source_str] = os.path.abspath(args.source)
+	spec_file_params[app_target_str] = target
 	spec_file_params[release_version_str] = args.version
 
 	print "#"*100
@@ -117,3 +128,5 @@ def main():
 	copy_files(spec_file_path, rpm_file_path)
 
 main()
+
+
