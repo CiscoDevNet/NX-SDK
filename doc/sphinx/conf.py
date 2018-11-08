7f710070626a6822cbe58e02a72f4dfe0d82d154
malrusha <malrusha@cisco.com>
2018-11-07 16:33:53 -0800

    Added doc

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -154,3 +154,31 @@ texinfo_documents = [
      author, 'NX-SDK', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+import subprocess, sys
+
+def run_doxygen(folder):
+    """Run the doxygen make command in the designated folder"""
+
+    try:
+        retcode = subprocess.call("cd %s; make" % folder, shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+    except OSError as e:
+        sys.stderr.write("doxygen execution failed: %s" % e)
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands if we're on the ReadTheDocs server"""
+
+    read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+    if read_the_docs_build:
+
+        run_doxygen("../../include")
+
+
+def setup(app):
+
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
