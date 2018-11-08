a38eaf945b02aa4d9f8837f82655a578cb65a416
malrusha <malrusha@cisco.com>
2018-11-07 16:30:29 -0800

    Added doc

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 #
 html_theme = 'alabaster'
 
-html_extra_path = "../html"
+html_extra_path = ["../html"]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
